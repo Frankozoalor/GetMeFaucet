8fe32adb7668ca43a57d2c59d32d344652f06615
Frankozoalor <frankozoalor@gmail.com>
2023-04-26 14:17:33 +0100

    React app code

---
+++ src/App.js
@@ -0,0 +1,133 @@
+import "./App.css";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
+import { abi, faucetContract } from "./constants";
+
+function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [withdrawError, setWithdrawError] = useState("");
+  const [withdrawSucess, setWithdrawSucess] = useState("");
+  const [transactionData, setTransactionData] = useState("");
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Change network to Goerli");
+      throw new Error("change network to Goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+
+    return web3Provider;
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getfaucet = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const faucetcontract = new Contract(faucetContract, abi, signer);
+      const getfaucet = await faucetcontract.getFaucet();
+      setWithdrawSucess("You have recieved some faucet!");
+      setTransactionData(getfaucet.hash);
+    } catch (err) {
+      setWithdrawError(err.message);
+      console.log(err);
+    }
+  };
+
+  const renderButton = () => {
+    if (walletConnected) {
+      return <button className="button is-link is-small"> Connected </button>;
+    } else {
+      return (
+        <button onClick={connectWallet} className="button is-link is-small">
+          Connect wallet
+        </button>
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [connectWallet]);
+
+  return (
+    <>
+      <div>
+        <nav className="navbar">
+          <div className="container">
+            <div className="navbar-brand">
+              <h1 className="navbar-item is-size-4">Faucet Token </h1>
+            </div>
+            <div id="navbarMenu" className="navbar-menu">
+              <div className="navbar-end is-align-items-center">
+                {renderButton()}
+              </div>
+            </div>
+          </div>
+        </nav>
+        <section className="hero is-fullheight">
+          <div className="faucet-hero-body">
+            <div className="container has-text-centered main-content">
+              <h1 className="title is-1">Faucet</h1>
+              <p className="color">Fast and reliable. 10 Faucet Token/day.</p>
+              <div className="box address-box">
+                <div className="columns">
+                  <div className="column is-four-fifths">
+                    <input
+                      className="input is-medium"
+                      type="text"
+                      placeholder="Enter your wallet address"
+                    />
+                  </div>
+                  <div className="column">
+                    <button
+                      className="button is-link is-medium"
+                      onClick={getfaucet}>
+                      GET FAUCET
+                    </button>
+                  </div>
+                </div>
+                <article className="panel is-grey-darker">
+                  <p className="panel-heading">Transaction Data</p>
+                  <div className="panel-block">
+                    <p>
+                      {" "}
+                      {transactionData
+                        ? `Transaction hash: ${transactionData}`
+                        : "--"}
+                    </p>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,226 @@
+const FaucetReserveContractAddress =
+  "0x9A1710f47Fae7e44777caFdfC88173c7912bDBac";
+
+const faucetContract = "0xa9321ee25Ed19F43AafB175431b6bCafC711cB0D";
+
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "faucetReserveAddress_",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Deposit",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "orderId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "address",
+        name: "requestor",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "OrderCreated",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "donateFaucet",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "faucetAmount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "faucetReserve",
+    outputs: [
+      {
+        internalType: "contract IERC20",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getBalance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getFaucet",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "lockTime",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "orderById",
+    outputs: [
+      {
+        internalType: "address",
+        name: "requestor",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "orderIdTracker",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "_value",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "setFaucetAmount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "time",
+        type: "uint256",
+      },
+    ],
+    name: "setLockTime",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
+
+module.exports = {
+  faucetContract,
+  FaucetReserveContractAddress,
+  abi,
+};
